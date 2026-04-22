aed3c2232f43b72547aca39823e6e221c600dd5e
Nicky Fang <nfang3885@gmail.com>
2026-04-22 13:15:42 -0400

    Added prompts from npm

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 require('dotenv').config();
 const { GoogleGenAI } = require("@google/genai");
+const prompts = require('prompts');
+prompts.override(require('yargs').argv);
 
-// The client automatically looks for GEMINI_API_KEY in your env
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 (async () => {
-    try {
-        const response = await ai.models.generateContent({
-            // Use the 3.1 Flash model for the best speed/intelligence balance
-            model: "gemini-3-flash-preview", 
-            contents: "What is the capital of Canada?",
-            config: {
-                // You can add your "friendly assistant" persona here
-                systemInstruction: "I am a friendly and helpful assistant. Ask me anything!"
-            }
+
+        const examplePrompt = await prompts({
+                type: 'text',
+                name: 'value',
+                message: 'What is your name?'
         });
 
-        console.log(response.text);
-    } catch (error) {
-        console.error("Error generating content:", error);
-    }
+        console.log(examplePrompt.value);
+
 })();
