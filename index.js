b9163be07c74e06acdd35ce0ecfbb35ad7dc462e
Nicky Fang <nfang3885@gmail.com>
2026-04-22 13:53:57 -0400

    Working basic overview of PDF summarizer

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ require('dotenv').config();
 const { GoogleGenAI } = require("@google/genai");
 const prompts = require('prompts');
 prompts.override(require('yargs').argv);
-const pdf = require('pdf-parse');
-
-//promise to await responses
 const fs = require('fs/promises');
 const path = require('path');
 
@@ -12,25 +9,52 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 (async () => {
 
-        const examplePrompt = await prompts({
-                type: 'text',
-                name: 'value',
-                message: 'What is your name?'
-        });
+    const files = await fs.readdir('./files');
+    // Filter for PDF files, case-insensitive, and ensure we only get pdf files
+    let pdfs = files.filter(file => path.extname(file).toLowerCase() === '.pdf');
 
-        const Files = await fs.readdir('./files');
-        // Filter for PDF files, case-insensitive, and ensure we only get pdf files
-        let pdfs = allFiles.filter(file => path.extname(file).toLowerCase() === '.pdf');
+    let choices = pdfs.map(file => ({ title: file, value: file }));
 
-        let choices = pdfs.map(file => ({ title: file, value: file }));
+    //user selects a pdf file to analyze
+    const pdfPrompt = await prompts({
+        type: 'select',
+        name: 'selectedPDF',
+        message: 'Select a PDF file to analyze:',
+        choices: choices
+    });
 
-        const pdfPrompt = await prompts({
-                type: 'select',
-                name: 'value',
-                message: 'Select a PDF file to analyze:',
-                choices: choices
-        });
+    // user types their questoon about the pdf
+    const pdfSelected = await prompts({
+        type: 'text',
+        name: 'userQuestion',
+        message: 'What do you want to know about the PDF?',
+    })
 
-        
+    const filePath = path.join('./files', pdfPrompt.selectedPDF);
+    
+    const uploadResult = await ai.files.upload({
+        file: filePath,
+        config: {
+            mimeType: "application/pdf",
+            displayName: pdfPrompt.selectedPDF,
+        }
+    });
+
+    const result = await ai.models.generateContent({
+        model: "gemini-3-flash-preview",
+        contents: [
+            {
+                fileData: {
+                    mimeType: uploadResult.mimeType,
+                    fileUri: uploadResult.uri,
+                },
+            },
+            { text: pdfSelected.userQuestion }, 
+        ],
+    });
+
+    // Print the result
+    console.log("\n--- ANSWER ---");
+    console.log(result.text);
 
 })();
